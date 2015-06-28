748ebf425f8af147215380fd5f3f722f32698ea7
Estel Smith <estel.smith@gmail.com>
2015-06-28 16:42:12 -0400

    Updated EOF entry to be a literal list.

--- src/Recognizer/Rule/Entry/Eof.php
+++ src/Recognizer/Rule/Entry/Eof.php
@@ -2,8 +2,15 @@
 
 namespace ESJ\Earley\Recognizer\Rule\Entry;
 
-class Eof implements Entry
+class Eof extends LiteralList
 {
+    public function __construct()
+    {
+        parent::__construct([
+            new Literal(chr(0))
+        ]);
+    }
+
     public function __toString()
     {
         return '\0';
