7a4068cb4fffc1e23522fe62bde38b740bfa8461
Estel Smith <estel.smith@gmail.com>
2015-06-27 21:37:17 -0400

    Added RuleCollection.

---
+++ src/Recognizer/Rule/RuleCollection.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace ESJ\Earley\Recognizer\Rule;
+
+use ESJ\Earley\ToString;
+
+class RuleCollection implements \IteratorAggregate, ToString
+{
+    /**
+     * @var Rule[]
+     */
+    private $rules = [];
+
+    /**
+     * @param Rule[] $rules
+     */
+    public function __construct(array $rules)
+    {
+        $this->addRules($rules);
+    }
+
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->rules);
+    }
+
+    public function __toString()
+    {
+        $rules = array_map(function (Rule $rule) {
+            return $rule->__toString();
+        }, $this->rules);
+
+        return implode("\n", $rules);
+    }
+
+    /**
+     * @param array $rules
+     */
+    private function addRules(array $rules)
+    {
+        foreach ($rules as $rule) {
+            $this->addRule($rule);
+        }
+    }
+
+    /**
+     * @param Rule $rule
+     */
+    private function addRule(Rule $rule)
+    {
+        $this->rules[] = $rule;
+    }
+}
